6a70b152be85465e4ea7dead2c7e4107c75ef6d0
nsheo <nsheo@ntels.com>
2022-10-20 15:27:44 +0900

    test

--- airflow_db_logger/consts.py
+++ airflow_db_logger/consts.py
@@ -28,11 +28,11 @@ def get_default_loggin_config():
 
     action_logging_config_env = os.environ.get(config_env_name, None)
 
-    os.environ[config_env_name] = "airflow_db_logger.shell_logging_config.SIMPLE_LOGGING_CONFIG"
+    os.environ[config_env_name] = "airflow_db_logger.shell_logging_config.LOGGING_CONFIG"
 
-    from airflow.config_templates.airflow_local_settings import DEFAULT_LOGGING_CONFIG  # noqa
+    #from airflow.config_templates.airflow_local_settings import DEFAULT_LOGGING_CONFIG  # noqa
 
     if action_logging_config_env is not None:
         os.environ[config_env_name] = action_logging_config_env
 
-    return DEFAULT_LOGGING_CONFIG
+    return DB_LOGGER_LOGGING_CONFIG
